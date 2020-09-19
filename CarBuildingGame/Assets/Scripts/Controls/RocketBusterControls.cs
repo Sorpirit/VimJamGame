e15616aec89028f3b13a3714b7c5fdc5b728fd57
Sorp <danvuk11@gmail.com>
2020-09-19 15:22:02 +0300

    add + rocket jumper

---
+++ CarBuildingGame/Assets/Scripts/Controls/RocketBusterControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBusterControls : MonoBehaviour,IActiveComponent
+{
+
+    private List<RocketJumper> jumpers;
+    private Rigidbody2D carBody;
+
+    private bool isFrozen;
+
+    private void Awake()
+    {
+        carBody = GetComponent<Rigidbody2D>();
+        jumpers = new List<RocketJumper>();
+
+        isFrozen = true;
+    }
+
+    private void Update()
+    {
+        if (isFrozen)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        foreach(RocketJumper jumper in jumpers)
+        {
+            jumper.Jump(carBody);
+        }
+    }
+
+    public void AddBuster(RocketJumper jumper)
+    {
+        jumpers.Add(jumper);
+    }
+
+    public void Freeze(bool val)
+    {
+        isFrozen = val;
+    }
+}
---
+++ CarBuildingGame/Assets/Scripts/RocketJumper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketJumper : MonoBehaviour
+{
+    [SerializeField] private float force;
+    [SerializeField] private float rechargeTime;
+
+    private float fireTimer;
+
+    private void Update()
+    {
+        if (fireTimer > 0)
+            fireTimer -= Time.deltaTime;
+    }
+
+    public void Jump(Rigidbody2D carBody)
+    {
+        if (fireTimer > 0)
+            return;
+
+        fireTimer = rechargeTime;
+        carBody.AddForceAtPosition(transform.up * force, transform.position);
+    }
+}
